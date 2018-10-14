28033bf709d63705b6176e2a7b77fd8e2a93a7d5
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 18:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
